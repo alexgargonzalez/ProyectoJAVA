90623d2c1e87adf39e93c1a02ca54e12751eeba8
alexdev <alexgargonzalez@hotmail.com>
2025-04-22 11:02:51 +0200

    prueba git

--- src/main/java/izv/zaidinvergeles/tienda/Tienda.java
+++ src/main/java/izv/zaidinvergeles/tienda/Tienda.java
@@ -3,7 +3,7 @@ public class Tienda{
     public static void main(String[] args) {
         Login inicio = new Login();
         inicio.setVisible(true);
-        System.out.println("Alex es gay");
+        System.out.println("Alfonso es gay");
         System.out.println("Alex es gay");
     }
 }
