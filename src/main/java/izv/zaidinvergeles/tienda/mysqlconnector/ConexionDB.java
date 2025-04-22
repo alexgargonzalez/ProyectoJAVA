c7487874b8461514feb659bba20e9da29e2d69a9
alexdev <alexgargonzalez@hotmail.com>
2025-04-22 19:53:53 +0200

    Conexion a base de datos mejorada

--- src/main/java/izv/zaidinvergeles/tienda/mysqlconnector/ConexionDB.java
+++ src/main/java/izv/zaidinvergeles/tienda/mysqlconnector/ConexionDB.java
@@ -1,29 +1,29 @@
 package izv.zaidinvergeles.tienda.mysqlconnector;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
 public class ConexionDB {
-    // Declaramos la conexion a mysql
+    // Declaramos la conexion a MySQL
     public static Connection con;
-    // Declaramos los datos de conexion a la bd
-    private static final String driver="com.mysql.jdbc.Driver";
-    private static final String user="root";
-    private static final String pass="1234";
-    private static final String url="jdbc:mysql://localhost:3306/login?characterEncoding=utf8";
-    // Funcion que va conectarse a mi bd de mysql
-    public Connection conectar(){
-      con = null;
-      try{
-          con = (Connection) DriverManager.getConnection(url, user, pass);
-          if(con!=null){
-          }
-      }
-      catch(SQLException e)
-      {
-          JOptionPane.showMessageDialog(null,"Error" + e.toString());
-      }
-      return con;
+    
+    // Datos de conexión a la BD
+    private static final String driver = "com.mysql.cj.jdbc.Driver";
+    private static final String user = "root";
+    private static final String pass = "1234";
+    private static final String url = "jdbc:mysql://localhost:3306/BD_Tienda?characterEncoding=utf8&useSSL=false&serverTimezone=UTC";
+    
+    // Función para conectarse a la BD
+    public Connection conectar() {
+        con = null;
+        try {
+            Class.forName(driver); // Cargar el driver
+            con = DriverManager.getConnection(url, user, pass);
+        } catch (ClassNotFoundException | SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error de conexión: " + e.toString());
+        }
+        return con;
     }
 }
