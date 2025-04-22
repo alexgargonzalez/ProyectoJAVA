e4fd01d1116f2add9f7610c285a5a087d9a1d235
alfonso <alfonso@gmail.com>
2025-04-22 10:34:14 +0200

    Alfonso

---
+++ src/main/java/izv/zaidinvergeles/tienda/Tienda.java
@@ -0,0 +1,8 @@
+package izv.zaidinvergeles.tienda;
+public class Tienda{
+    public static void main(String[] args) {
+        Login inicio = new Login();
+        inicio.setVisible(true);
+        System.out.println("Alex es gay");
+    }
+}
