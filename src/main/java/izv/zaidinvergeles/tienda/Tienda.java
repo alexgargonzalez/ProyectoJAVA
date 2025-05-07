a355a765ce46f0060a52552b6b085b51c5bd0d2e
Alejandro Garcia Gonzalez <alexgargonzalez@hotmail.com>
2025-05-07 09:57:01 +0200

    Delete src/main directory

--- src/main/java/izv/zaidinvergeles/tienda/Tienda.java
+++
@@ -1,9 +0,0 @@
-package izv.zaidinvergeles.tienda;
-public class Tienda{
-    public static void main(String[] args) {
-        Login inicio = new Login();
-        inicio.setVisible(true);
-        inicio.setLocationRelativeTo(null);
-        
-    }
-}
